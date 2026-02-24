68b8e5cbd7eeb39b8e508ffc3148cbdf6937f628
Bruno Gomes <gomess.hbruno@gmail.com>
2026-02-23 22:26:45 -0300

    add(index): new rules

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -3,10 +3,20 @@ import { noMagicNumbers } from './no-magic-numbers';
 import { noConsole } from './no-console';
 import { noTodoComments } from './no-todo-comments';
 import { deepNesting } from './no-deep-nesting';
+import { noElse } from './no-else';
+import { noAsyncInFor } from './no-async-in-for';
+import { noSequentialAwaits } from './no-sequential-awaits';
+import { noNestedArrayMethods } from './no-nested-array-methods';
+import { noChainedFilters } from './no-chained-filters';
 
 export const allRules: Rule[] = [
   noMagicNumbers,
   noConsole,
   noTodoComments,
   deepNesting,
+  noElse,
+  noAsyncInFor,
+  noSequentialAwaits,
+  noNestedArrayMethods,
+  noChainedFilters,
 ];
