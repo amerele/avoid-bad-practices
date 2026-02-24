6e06c11e33ca76375d25b12d1ad304a086685847
Bruno Gomes <gomess.hbruno@gmail.com>
2026-02-23 21:57:52 -0300

    fix(deep-nesting): renaming file

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -2,7 +2,7 @@ import { Rule } from './rule';
 import { noMagicNumbers } from './no-magic-numbers';
 import { noConsole } from './no-console';
 import { noTodoComments } from './no-todo-comments';
-import { deepNesting } from './deep-nesting';
+import { deepNesting } from './no-deep-nesting';
 
 export const allRules: Rule[] = [
   noMagicNumbers,
